3566d08f378e8ec55570a1dff52dffe23486510b
Taras <taras.sgibnev@gmail.com>
2025-01-10 22:42:32 +0200

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,25 @@
 // JavaScript Document
 
-// DOM is ready
+window.onload = swap;
+
+function swap() {
+	var numimages = 6;
+	var x = (Math.floor(Math.random() * numimages));
+	var randomimage = new Array(
+		"images/logo/logo1.svg",
+		"images/logo/logo2.svg",
+		"images/logo/logo3.svg",
+		"images/logo/logo4.svg",
+		"images/logo/logo5.svg",
+		"images/logo/logo6.svg"
+	);
+	randomimage=(randomimage[x]);
+	document.getElementsByClassName("logo")[0].style.backgroundImage = "url("+ randomimage +")";	
+}
+
+
+/*
+
 $(document).ready(function() {
 
 	'use strict';
@@ -8,3 +27,4 @@ $(document).ready(function() {
 	document.document.getElementsByClassName('autoplay').play();
 
 });
+*/
